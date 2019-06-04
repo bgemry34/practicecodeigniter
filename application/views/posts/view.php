b45c23b5deeba076a3a78fbc67069e9482e16f83
Gemry <bgemry@yahoo.com>
2019-06-04 12:40:41 +0800

    part 5

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -11,3 +11,37 @@
 </div>
 
 <a href="<?=base_url().'posts/edit/'.$post['slug'];?>" class="btn btn-default pull-right">Edit</a>
+<br><hr style="background-color: black;height: 1px;">
+<h3>Comments</h3>
+<hr style="background-color: black;height: 1px;">
+<?php if($comments):?>
+	<div class="row">
+	<ul class="list-group">
+	<?php foreach($comments as $comment): ?>
+		<li class="list-group-item well">
+		<h5><?=$comment['body'];?>[by <strong><?=$comment['name'];?></strong>]</h5>
+		</li>
+	<?php endforeach;?>
+	</ul>
+	</div>
+<?php else:?>
+	<p>No comment to Dispay</p>
+<?php endif;?>
+<h3 style="text-align: center;">Add Comment</h3>
+<?=validation_errors();?>
+<?=form_open('comments/create/'.$post['id'])?>
+<div class="form-group">
+	<label>Name:</label>
+	<input type="text" name="name" class="form-control">
+</div>
+<div class="form-group">
+	<label>Email:</label>
+	<input type="email" name="userEmail" class="form-control">
+</div>
+<div class="form-group">
+	<label for="">Comment:</label>
+	<textarea name="body" class=form-control></textarea>
+</div>
+<input type="hidden" name="slug" value="<?=$post['slug'];?>">
+<input type="submit" value="Submit" class="btn btn-primary pull-right">
+</form>
